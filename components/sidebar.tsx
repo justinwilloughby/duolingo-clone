d427d7ad9e3e75de94a53b55f9570e8533c8937b
Justin Willoughby <willoughby0114@gmail.com>
2024-04-09 14:40:26 -0500

    Landing page and authentication.

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -2,6 +2,8 @@ import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import { SidebarItem } from "./sidebaritem";
+import { ClerkLoaded, ClerkLoading, UserButton } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
 
 type Props = {
     className?: string;
@@ -26,6 +28,14 @@ export const Sidebar = ({ className }: Props) => {
                 <SidebarItem label="Leaderboard" href="/leaderboard" iconSrc="/leaderboard.svg" />
                 <SidebarItem label="Quests" href="/quests" iconSrc="/quests.svg" />
                 <SidebarItem label="Shop" href="/shop" iconSrc="/shop.svg" />
+            </div>
+            <div className="p-4">
+                <ClerkLoading>
+                    <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+                </ClerkLoading>
+                <ClerkLoaded>
+                    <UserButton afterSignOutUrl="/" />
+                </ClerkLoaded>
             </div>
         </div>
     );
