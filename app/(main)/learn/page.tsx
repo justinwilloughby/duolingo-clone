480e4ba591e9acc1e1f019897d5fb60aee802fa4
Justin Willoughby <willoughby0114@gmail.com>
2024-04-08 21:08:47 -0500

    Sidebar items, and learn skeleton.

---
+++ app/(main)/learn/page.tsx
@@ -0,0 +1,22 @@
+import { FeedWrapper } from "@/components/feedwrapper";
+import { StickyWrapper } from "@/components/stickywrapper";
+import { Header } from "./header";
+import { UserProgress } from "@/components/userprogress";
+
+export default function Home() {
+  return (
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        <UserProgress 
+          activeCourse={{ title: "Spanish", imageSrc: "./es.svg" }}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header title="Spanish" />
+      </FeedWrapper>
+    </div>
+  );
+};
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,4 +1,7 @@
 import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { SidebarItem } from "./sidebaritem";
 
 type Props = {
     className?: string;
@@ -7,10 +10,23 @@ type Props = {
 export const Sidebar = ({ className }: Props) => {
     return (
         <div className={cn(
-            "flex h-full lg:w-[256px] bg-blue-500 lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
+            "flex h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
             className,
         )}>
-            SideBar
+            <Link href="/">
+                <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+                    <Image src="./mascot.svg" height={40} width={40} alt="Mascot" />
+                    <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+                        Lingo
+                    </h1>
+                </div>
+            </Link>
+            <div className="flex flex-col gap-y-2 flex-1">
+                <SidebarItem label="Learn" href="/" iconSrc="/learn.svg" />
+                <SidebarItem label="Leaderboard" href="/leaderboard" iconSrc="/leaderboard.svg" />
+                <SidebarItem label="Quests" href="/quests" iconSrc="/quests.svg" />
+                <SidebarItem label="Shop" href="/shop" iconSrc="/shop.svg" />
+            </div>
         </div>
     );
 };
