52d43549fdb1cb763ee633741886f3e58c3c9d07
Justin Willoughby <willoughby0114@gmail.com>
2024-06-04 16:04:00 -0500

    Neon + Drizzle. Course selection.

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -2,8 +2,18 @@ import { FeedWrapper } from "@/components/feedwrapper";
 import { StickyWrapper } from "@/components/stickywrapper";
 import { Header } from "./header";
 import { UserProgress } from "@/components/userprogress";
+import { getUserProgress } from "@/db/queries";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const userProgressData = getUserProgress();
+
+  const [userProgress] = await Promise.all([userProgressData]);
+
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
+  }
+
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
