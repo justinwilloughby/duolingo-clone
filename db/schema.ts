3c14af0fb0ad3316cbe67be636012b7269c1f009
Justin Willoughby <willoughby0114@gmail.com>
2024-06-10 09:56:59 -0500

    Schema bugfix.

--- db/schema.ts
+++ db/schema.ts
@@ -62,7 +62,7 @@ export const challengesRelations = relations(challenges, ({ one, many }) => ({
     challengeProgress: many(challengeProgress),
 }));
 
-export const challengeOptions = pgTable("challengeOptions", {
+export const challengeOptions = pgTable("challenge_options", {
     id: serial("id").primaryKey(),
     challengeId: integer("challenge_id").references(() => challenges.id, { onDelete: "cascade" }).notNull(),
     text: text("text").notNull(),
@@ -78,7 +78,7 @@ export const challengeOptionsRelations = relations(challengeOptions, ({ one }) =
     }),
 }));
 
-export const challengeProgress = pgTable("challengeProgress", {
+export const challengeProgress = pgTable("challenge_progress", {
     id: serial("id").primaryKey(),
     userId: text("user_id").notNull(),
     challengeId: integer("challenge_id").references(() => challenges.id, { onDelete: "cascade" }).notNull(),
@@ -92,7 +92,7 @@ export const challengeProgressRelations = relations(challengeProgress, ({ one })
     }),
 }));
 
-export const userProgress = pgTable("userProgress", {
+export const userProgress = pgTable("user_progress", {
     userId: text("user_id").primaryKey(),
     userName: text("user_name").notNull().default("User"),
     userImageSrc: text("user_image_src").notNull().default("/mascot.svg"),
